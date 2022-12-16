6cce6f2679c8d7dcd1c5c0fb40f0dd0ce68e7641
Wertolet9999 <wertolet9999@gmail.com>
2022-12-16 09:59:52 +0200

    add task_2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const throttledPlay = throttle((event) => {
+  localStorage.setItem('videoplayer-current-time', event.seconds); 
+  // створює новий, або оновлює вже існуючий запис у сховищі
+  // throttle контролює кількість разів, яку функція може бути викликана протягом 1c
+}, 1000);
+player.on('timeupdate', throttledPlay);
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+// відновлення відтворення зі збереженої позиції
